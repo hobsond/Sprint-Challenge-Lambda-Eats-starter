e8741574a5ea64f4ac4ddf73f09bde705580408a
unknown <hobsond26@icloud.com>
2020-04-24 14:53:50 -0400

    setup yup validation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React,{useState,useEffect}from "react";
 import {Route,Switch,Link} from 'react-router-dom'
+import axios from 'axios'
+import * as yup from 'yup'
+
 
 import Homepage from "./comoponents/Homepage"
 import PizzaForm from "./comoponents/PizzaForm"
 
 const initialValues = {
+  name :'',
   size :'',
   sauceChoice:'',
   addToppings:'',
   specialChoice:{
-    peperoni:'',
-    sausage:'',
-    cheese:'',
-    pineapple:'',
+    peperoni:false,
+    sausage:false,
+    cheese:false,
+    pineapple:false,
   },
   specialInstructions:'',
   quantity:'',
@@ -23,15 +27,44 @@ const initialValues = {
 
 
 
+
 const App = () => {
   const [formData, setFormData]=useState(initialValues)
+  const [formErrors,setFormErrors] = useState({
+    name:'',
+  })
+  
+  const [priceTotal,setPriceTotal]=useState(0)
+
+  const formScheme = yup.object().shape({
+    name:yup
+    .string()
+    .required('You Must Include Name')
+    .min(5,'Name must be atleast 5 characters')
+  
+  })
 
   const onChangeHandle = (evt)=>{
     const name = evt.target.name;
     const value = evt.target.value;
-    setFormData({...formData,
-    [name] :value
+    yup
+    .reach(formScheme,name)
+    .validate(value)
+    .then(valid=>{
+      setFormErrors({...formErrors,
+      [name]: '',
+      })
+    
     })
+    .catch(err=>{
+      setFormErrors({...formErrors,
+      [name]:err.errors[0]
+      })
+    })
+    setFormData({...formData,
+      [name] :value
+      })
+    
   
   }
 
@@ -39,11 +72,31 @@ const App = () => {
     const name = evt.target.name;
     const checked = evt.target.checked;
     setFormData({...formData,
-    ...formData.specialChoice,
-      [name] : checked
+      specialChoice:{...formData.specialChoice,
+        [name] : checked
+      
+      }
+  
+      
     })
 
   }
+
+  const onSubmitHandle = evt=>{
+    evt.preventDefault()
+
+    axios.post('https://reqres.in/',formData)
+    .then(res=>{
+      console.log(res.data)
+    })
+    .catch(err => console.log(err))
+
+    setFormData(initialValues)
+
+  }
+
+
+  
 
   return (
     <div>
@@ -57,6 +110,7 @@ const App = () => {
           formData={formData} 
           onChangeHandle={onChangeHandle}
           onChecked={onChecked}
+          formErrors={formErrors}
           />
         </Route>
       
--- src/comoponents/PizzaForm.js
+++ src/comoponents/PizzaForm.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 export default function PizzaForm(props) {
-    const {onChangeHandle,onChecked,formData} = props
+    const {onChangeHandle,onChecked,formData,formErrors} = props
+
+    
     return (
         <form>
+            <label htmlFor='name'>Please Enter A Name for Order</label>
+            <p>{formErrors.name} </p>
+    
+            <input name='name' type='text' onChange={onChangeHandle}/>
             <label htmlFor='size'>Choose Your Size</label>
             <select name='size' onChange={onChangeHandle}>
                 <option>Please Select A Size</option>
@@ -35,6 +41,15 @@ export default function PizzaForm(props) {
 
             <label htmlFor='pineapple'>Pineapple</label>
             <input onChange={onChecked} type='checkbox' name='pineapple'/>
+
+            <h4>Special Instructions</h4>
+
+            <input type='textarea' name='specialInstructions' onChange={onChangeHandle} />
+
+            <label htmlFor='quantity'>quantity</label>
+            <input type='number' step='1' name='quantity' onChange={onChangeHandle}/>
+
+            <button>Order Now</button>
 
 
 
